9b835e9340ba5f96899e149b6fdb830bc32b13bf
Alessandra Soares dos Santos <AlessandraSoaresdosSantos@users.noreply.github.com>
2021-10-26 19:38:44 -0300

    adequações

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-const contador = document.getElementById("contador");
+const resultado = document.getElementById("resultado");
 
-contador.addEventListener(
-  "click", // tipo do evento
-  (event) => {
-    event.target.innerHTML++;
-  }
-);
-/*
-- adiciona um "escutador" ao evento de click
-  do elemento contador
-
-- o tipo do evento "click"
-
-- o objeto do "event" vem por parametro
-  contém informações do evento
-
-- target é o elemento "alvo"
-  que sofreu o evento
-
-- innerHTML é o conteúdo do target
-
-- ++ é um operador que soma 1
-  a uma variavel numerica
-  ou a uma string de numeros
-
-*/
+ 
