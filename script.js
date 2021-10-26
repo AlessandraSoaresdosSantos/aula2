b0a01c7c6c2318ffb4287b11f0f082fffc7608c8
Alessandra Soares dos Santos <AlessandraSoaresdosSantos@users.noreply.github.com>
2021-10-26 20:32:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const resultado = document.getElementById("resultado");
 
- 
+var nome = "Alessandra";
+var sobrenome = "Santos";
+var nomeCompleto = `${nome}  ${sobrenome}`;
+var idade = window.prompt('Digite a sua idade');
+
+if (isNaN(idade))
+{
+   resultado.innerHTML = 'não é número';
+}
+else
+{ 
+  if(idade >= 18)
+  {
+    resultado.innerHTML = "O usuário" + nomeCompleto + " é adulto";
+   
+  }
+  else
+  {
+    resultado.innerHTML = "O usuário" + nomeCompleto + " é menor de idade";
+  }
+} 
